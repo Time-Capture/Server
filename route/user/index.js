5692e4ccaa20156a437743f09e66ffd1679bf8b7
syeutyu <syeuty@naver.com>
2017-12-16 17:43:46 +0900

    [COMPLEATE] Sign_In and Sign_Up

---
+++ route/user/index.js
@@ -0,0 +1,45 @@
+const router = require('express').Router();
+const user = require('../../database/schema/User');
+const uuid = require('uuid/v4');
+const controller = require('./user_controller');
+
+router.route('/signup').post((req, res) => {
+    console.log('유저 회원가입');
+    let id = req.body.id;
+    let pw = req.body.password;
+    let name = req.body.name;
+    user.findOne({
+            'id': id,
+            'password': pw
+        })
+        .then((f_user) => {
+            if (f_user) {
+                res.status(400).json({
+                    'msg': 'Not Accept Request'
+                }).end();
+            } else {
+                let obj = new user({
+                    'id': id,
+                    'password': pw,
+                    'name': name,
+                    'uuid': uuid()
+                });
+                obj.save(() => {
+                    console.log(id + 'DB Save');
+                    res.status(200).end();
+                });
+            }
+        });
+});
+
+router.route('/signin').post((req, res) => {
+    console.log('유저 로그인');
+    let id = req.body.id;
+    let pw = req.body.password;
+    user.findOne({ 'id': id, 'password': pw })
+        .then((f_user) => {
+            { f_user ? res.status(200).json({ 'token': f_user.uuid }).end() : res.status(400).end() }
+        })
+});
+
+module.exports = router;
